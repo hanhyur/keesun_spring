aa747d879362edb870ccd41d9696407beadab903
gracenam <thankowl777@kakao.com>
2022-08-22 15:01:29 +0900

    feature : 스프링 웹 MVC demo
    <description>
    * 스프링 웹 MVC
    - 스프링 MVC 모델
    - 서블릿 생명주기
    
    <issue-number>

---
+++ demo-spirng-mvc/src/main/java/me/gracenam/demospirngmvc/DemoSpirngMvcApplication.java
@@ -0,0 +1,13 @@
+package me.gracenam.demospirngmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpirngMvcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSpirngMvcApplication.class, args);
+  }
+
+}
---
+++ demo-spirng-mvc/src/test/java/me/gracenam/demospirngmvc/DemoSpirngMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gracenam.demospirngmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpirngMvcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
