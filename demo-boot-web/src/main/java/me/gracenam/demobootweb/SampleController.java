2769e0e3e7b61d8b74056f841b712c49efb13d3d
gracenam <thankowl777@kakao.com>
2022-08-26 15:36:58 +0900

    feature : 핸들러 인터셉터
    <description>
    * 스프링 웹 MVC
    - 핸들러 인터셉터 구현
    
    <issue-number>

--- demo-boot-web/src/main/java/me/gracenam/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/me/gracenam/demobootweb/SampleController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+  // preHandle
+  // 요청 처리
+  // postHandle
+  // 뷰 렌더링
+  // afterCompletion
+
   @GetMapping("/hello")
   public String hello(@RequestParam("id") Person person) {
     return "hello " + person.getName();
