1c1ce868b458316c049681f513f1ba99858f7b79
gracenam <thankowl777@kakao.com>
2022-08-26 17:05:27 +0900

    feature : 리소스 핸들러
    <description>
    * 스프링 웹 MVC
    - 정적 리소스 관리
    - 리소스 핸들러 설정
    
    <issue-number>

--- demo-boot-web/src/test/java/me/gracenam/demobootweb/SampleControllerTest.java
+++ demo-boot-web/src/test/java/me/gracenam/demobootweb/SampleControllerTest.java
@@ -1,16 +1,16 @@
 package me.gracenam.demobootweb;
 
-import org.aspectj.weaver.patterns.PerObject;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@WebMvcTest
 @SpringBootTest
@@ -35,4 +35,20 @@ class SampleControllerTest {
         .andExpect(content().string("hello spring"));
   }
 
+  @Test
+  public void helloStatic() throws Exception {
+    this.mockMvc.perform(get("/index.html"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string(Matchers.containsString("hello static")));
+  }
+
+  @Test
+  public void helloMobile() throws Exception {
+    this.mockMvc.perform(get("/mobile/index.html"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string(Matchers.containsString("hello mobile")))
+        .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+  }
 }
