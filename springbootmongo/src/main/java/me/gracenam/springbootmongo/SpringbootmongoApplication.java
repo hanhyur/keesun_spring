c1137117b69734495cf81450ec1c9258efc2db9c
gracenam <thankowl777@kakao.com>
2022-09-05 18:02:39 +0900

    feature : MongoDB
    <description>
    - 스프링 데이터
    >> MongoDB 데이터 입력 및 Repository 사용
    
    <issue-number>

--- springbootmongo/src/main/java/me/gracenam/springbootmongo/SpringbootmongoApplication.java
+++ springbootmongo/src/main/java/me/gracenam/springbootmongo/SpringbootmongoApplication.java
@@ -1,13 +1,38 @@
 package me.gracenam.springbootmongo;
 
+import me.gracenam.springbootmongo.account.Account;
+import me.gracenam.springbootmongo.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class SpringbootmongoApplication {
 
+  @Autowired
+  MongoTemplate mongoTemplate;
+
+  @Autowired
+  AccountRepository accountRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(SpringbootmongoApplication.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner applicationRunner() {
+    return args -> {
+      Account account = new Account();
+      account.setEmail("grace@email.com");
+      account.setUsername("grace");
+      accountRepository.insert(account);
+
+      System.out.println("finished");
+    };
   }
 
 }
---
+++ springbootmongo/src/main/java/me/gracenam/springbootmongo/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package me.gracenam.springbootmongo.account;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+}
