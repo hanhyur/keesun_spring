882a7cfb578d5c7c8b2d163747dcca4098e16fa0
gracenam <thankowl777@kakao.com>
2022-09-06 16:30:54 +0900

    feature : neo4j 레포지토리
    <description>
    - neo4j
    >> 레포지토리 사용
    
    <issue-number>

--- springbootneo4j/src/main/java/me/gracenam/springbootneo4j/Neo4jRunner.java
+++ springbootneo4j/src/main/java/me/gracenam/springbootneo4j/Neo4jRunner.java
@@ -1,6 +1,7 @@
 package me.gracenam.springbootneo4j;
 
 import me.gracenam.springbootneo4j.account.Account;
+import me.gracenam.springbootneo4j.account.AccountRepository;
 import me.gracenam.springbootneo4j.account.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.core.Neo4jTemplate;
@@ -11,21 +12,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class Neo4jRunner implements ApplicationRunner {
 
+//  @Autowired
+//  Neo4jTemplate neo4jTemplate;
+
   @Autowired
-  Neo4jTemplate neo4jTemplate;
+  AccountRepository accountRepository;
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
     Account account = new Account();
-    account.setEmail("grace@email.com");
-    account.setUsername("grace");
+    account.setEmail("gracenam@email.com");
+    account.setUsername("gracenam");
 
     Role role = new Role();
-    role.setName("admin");
+    role.setName("user");
 
     account.getRoles().add(role);
 
-    neo4jTemplate.save(account);
+//    neo4jTemplate.save(account);
+
+    accountRepository.save(account);
 
     System.out.println("finished");
   }
