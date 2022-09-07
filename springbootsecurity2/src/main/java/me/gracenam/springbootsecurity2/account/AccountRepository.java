2e160b405ef5cdc04f2a47f3f7b6c020b43308a5
gracenam <thankowl777@kakao.com>
2022-09-07 16:12:32 +0900

    feature : UserDetailsService 구현
    <description>
    - 시큐리티 커스텀
    >> UserDetailsService 구현
    
    <issue-number>

--- springbootsecurity2/src/main/java/me/gracenam/springbootsecurity2/account/AccountRepository.java
+++ springbootsecurity2/src/main/java/me/gracenam/springbootsecurity2/account/AccountRepository.java
@@ -2,5 +2,8 @@ package me.gracenam.springbootsecurity2.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+  Optional<Account> findByName(String username);
 }
--- springbootsecurity2/src/main/java/me/gracenam/springbootsecurity2/account/AccountService.java
+++ springbootsecurity2/src/main/java/me/gracenam/springbootsecurity2/account/AccountService.java
@@ -1,4 +1,42 @@
 package me.gracenam.springbootsecurity2.account;
 
-public class AccountService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+  @Autowired
+  private AccountRepository accountRepository;
+
+  public Account createAccount(String name, String password) {
+    Account account = new Account();
+    account.setName(name);
+    account.setPassword(password);
+    return accountRepository.save(account);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<Account> byName = accountRepository.findByName(username);
+
+    Account account = byName.orElseThrow(() -> new UsernameNotFoundException(username));
+
+    return new User(account.getName(), account.getPassword(), authorities());
+  }
+
+  private Collection<? extends GrantedAuthority> authorities() {
+    return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
 }
