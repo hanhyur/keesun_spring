67efb70abb0ac0fa6eb9f045f2efd039e30f76b1
gracenam <thankowl777@kakao.com>
2022-09-07 11:32:13 +0900

    feature : 시큐리티 프로젝트 생성
    <description>
    - 스프링 시큐리티
    >> 시큐리티 프로젝트 생성
    
    <issue-number>

---
+++ springbootsecurity/src/test/java/me/gracenam/springbootsecurity/SpringbootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gracenam.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
