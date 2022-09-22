085621c8a74edeaa5881a510a9451dcc40fbe177
gracenam <thankowl777@kakao.com>
2022-09-22 16:52:41 +0900

    feature : bad request 처리
    <description>
    - rest api
    >> bad request 처리 - 값이 잘못된 경우
    >> 애노테이션으로 처리하기 어려운 경우 -> validator 생성
    >> 값에 따라 에러 메시지 출력되도록 작성
    
    <issue-number>

--- rest-api-with-spring/src/test/java/me/gracenam/restapiwithspring/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/gracenam/restapiwithspring/events/EventControllerTests.java
@@ -95,4 +95,27 @@ public class EventControllerTests {
                 )
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 9, 25, 11, 48))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 9, 24, 11, 48))
+                .beginEventDateTime(LocalDateTime.of(2022, 9, 23, 11, 48))
+                .endEventDateTime(LocalDateTime.of(2022, 9, 22, 11, 48))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(eventDto))
+                )
+                .andExpect(status().isBadRequest());
+    }
+
 }
