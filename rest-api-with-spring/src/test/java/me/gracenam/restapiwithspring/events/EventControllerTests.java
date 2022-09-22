e1e49af97403b5f2620db43db504a0beed7d1fc9
gracenam <thankowl777@kakao.com>
2022-09-22 11:57:02 +0900

    feature : 201 응답 확인
    <description>
    - rest api
    >> EventControllerTests 테스트 작성
    
    <issue-number>

--- rest-api-with-spring/src/test/java/me/gracenam/restapiwithspring/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/gracenam/restapiwithspring/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package me.gracenam.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,7 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,13 +21,32 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 9, 22, 11, 48))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 9, 23, 11, 48))
+                .beginEventDateTime(LocalDateTime.of(2022, 9, 24, 11, 48))
+                .endEventDateTime(LocalDateTime.of(2022, 9, 25, 11, 48))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
                 )
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 
 }
