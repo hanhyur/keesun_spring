74b969b23e99a67974c50ff06699412633eee522
gracenam <thankowl777@kakao.com>
2022-09-23 16:05:42 +0900

    feature : 스프링 REST Docs 적용
    <description>
    - rest api
    >> 작성한 스니펫으로 실제 문서 생성
    >> bootjar로 실제 문서 생성
    - test
    >> profile link 추가
    
    <issue-number>

--- rest-api-with-spring/src/main/java/me/gracenam/restapiwithspring/events/EventController.java
+++ rest-api-with-spring/src/main/java/me/gracenam/restapiwithspring/events/EventController.java
@@ -1,6 +1,7 @@
 package me.gracenam.restapiwithspring.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -53,7 +54,7 @@ public class EventController {
 
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
-
+        eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 
