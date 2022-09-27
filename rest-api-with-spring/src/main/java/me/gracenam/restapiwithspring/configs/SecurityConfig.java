7b864a007737f5c900d2a94b1621bfdfd43e4122
gracenam <thankowl777@kakao.com>
2022-09-27 11:45:39 +0900

    feature : 폼 인증 설정
    <description>
    - rest api
    >> 시큐리티 설정
    >> tokenStore 사용을 위한 의존성 주입
    >> org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure
    >> Oauth2 시큐리티 의존성 강제 주입
    >> tokenStore는 실제론 deprecated 되었음
    
    <issue-number>

--- rest-api-with-spring/src/main/java/me/gracenam/restapiwithspring/configs/SecurityConfig.java
+++ rest-api-with-spring/src/main/java/me/gracenam/restapiwithspring/configs/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +25,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     PasswordEncoder passwordEncoder;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
 
     @Bean
     @Override
